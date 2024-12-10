64302764bec37fbe76f30c27f1d8fd7dcbed0bad
Hunter Korczykowski <hunterkorczykowski@gmail.com>
2024-12-10 14:35:47 -0500

    Initial Commit

---
+++ NewsAPI2/NewsAPI2App.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPI2App.swift
+//  NewsAPI2
+//
+//  Created by Hunter Korczykowski (student LM) on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
